848c6f1ac304ee459f0a953d295da53f992e6527
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-03-04 14:53:48 +0100

    Feat: add data verification

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,22 +7,90 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// Elements du DOM
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-// launch modal event
+// Evenement au click qui lance la modale 
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+// Fonction pour le lancement de la modale
 function launchModal() {
   modalbg.style.display = "block";
 }
 
-
-// Fermer la modale 
+// Fonction pour la fermeture de la modale 
 function closeModal() {
   modalbg.style.display = "none";
 }
 
+//Récupération des champs 
+let first = document.getElementById('first');
+let last = document.getElementById('last');
+let email = document.getElementById('email');
+let radios = document.getElementsByClassName('radio');
+let cgu = document.getElementById('checkbox1')
+
+
+// Regex 
+const regexSupTwo = new RegExp('[a-zA-Z]{2,}');
+const regexEmail = new RegExp('^[a-z0-9.-_]{2,}@[a-z]{2,}\\.[a-z]{2,4}$')
+
+//Verification du prenom, + de 2 lettres et non vide
+first.addEventListener('input', function(){
+if (first.value!="" && regexSupTwo.test(first.value)) {
+  return true;
+} else {
+  return false;
+} 
+})
+
+//Verification du nom, + de 2 lettres et non vide
+last.addEventListener('input', function(){
+  if (last.value!="" && regexSupTwo.test(last.value)) {
+    return true;
+  } else {
+    return false;
+  }
+  })
+
+  //Vérification de l'adresse email 
+  email.addEventListener('input', function(){
+    if (email.value!="" && regexEmail.test(email.value)) {
+      return true;
+    } else {
+      return false;
+    }
+    })
+
+
+// Vérification des boutons radios
+for (let i=0; i<7; i++){
+  radios[i].addEventListener('input', function(){
+    if(radios[i].checked){
+      return true;
+    }
+    else {
+      return false;
+    }
+  })
+}
+
+// Vérification de la validation des CGU
+cgu.addEventListener('input', function(){
+  if (cgu.checked) {
+    return true;
+  } else {
+    return false;
+  }
+  })
+
+// Validation du formulaire 
+function validate(){
+  if (validateCgu){
+    return true;
+  }else {
+    return false
+  }
+}
