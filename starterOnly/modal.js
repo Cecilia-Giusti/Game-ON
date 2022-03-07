b5dae285f47a54fb0e003cada262d5c3906d30b9
Cecilia Giusti <ceciliagiusti2b@gmail.com>
2022-03-07 11:24:16 +0100

    Feat : add validation form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -25,41 +25,86 @@ function closeModal() {
   modalbg.style.display = "none";
 }
 
+
+
+
 //Récupération des champs 
 let first = document.getElementById('first');
 let last = document.getElementById('last');
 let email = document.getElementById('email');
+let birthdate = document.getElementById('birthdate');
 let radios = document.getElementsByClassName('radio');
-let cgu = document.getElementById('checkbox1')
+let cgu = document.getElementById('checkbox1');
+let form = document.getElementsByTagName('form');
 
 
 // Regex 
 const regexSupTwo = new RegExp('[a-zA-Z]{2,}');
-const regexEmail = new RegExp('^[a-z0-9.-_]{2,}@[a-z]{2,}\\.[a-z]{2,4}$')
+const regexEmail = new RegExp('^[a-z0-9.-_]{2,}@[a-z]{2,}\\.[a-z]{2,4}$');
+const regexBirthdate = new RegExp('[0-9]{2}\\/[0-9]{2}\\/[0-9]{4}');
 
-//Verification du prenom, + de 2 lettres et non vide
-first.addEventListener('input', function(){
-if (first.value!="" && regexSupTwo.test(first.value)) {
-  return true;
-} else {
+
+// Validation du formulaire
+
+function validate(){
+  if (first.value!="" && regexSupTwo.test(first.value)) {
+    if (last.value!="" && regexSupTwo.test(last.value)) {
+      if (email.value!="" && regexEmail.test(email.value)) {
+        if (birthdate.value!="" && regexBirthdate.test(birthdate.value)) {
+          if (cgu.checked) {
+            for (let i=0; i<7; i++){
+              if(radios[i].checked){
+                return true;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   return false;
-} 
-})
+}
+
+
+
+  //Verification du prenom, + de 2 lettres et non vide
+   first.addEventListener('input', function(){
+   if (first.value!="" && regexSupTwo.test(first.value)) {
+     return true;
+   } else {
+     return false;
+   } 
+   })
+ 
 
 //Verification du nom, + de 2 lettres et non vide
 last.addEventListener('input', function(){
   if (last.value!="" && regexSupTwo.test(last.value)) {
-    return true;
+    console.log(true);
   } else {
-    return false;
+    console.log(false);
   }
   })
 
   //Vérification de l'adresse email 
   email.addEventListener('input', function(){
     if (email.value!="" && regexEmail.test(email.value)) {
+      console.log(true);
       return true;
     } else {
+      console.log(false)
+      return false;
+    }
+    })
+
+
+    //Vérification de la date d'anniversaire 
+  birthdate.addEventListener('input', function(){
+    if (birthdate.value!="" && regexBirthdate.test(birthdate.value)) {
+      console.log(true);
+      return true;
+    } else {
+      console.log(false)
       return false;
     }
     })
@@ -69,9 +114,11 @@ last.addEventListener('input', function(){
 for (let i=0; i<7; i++){
   radios[i].addEventListener('input', function(){
     if(radios[i].checked){
+      console.log(true);
       return true;
     }
     else {
+      console.log(false)
       return false;
     }
   })
@@ -80,17 +127,10 @@ for (let i=0; i<7; i++){
 // Vérification de la validation des CGU
 cgu.addEventListener('input', function(){
   if (cgu.checked) {
+    console.log(true);
     return true;
   } else {
+    console.log(false);
     return false;
   }
   })
-
-// Validation du formulaire 
-function validate(){
-  if (validateCgu){
-    return true;
-  }else {
-    return false
-  }
-}
